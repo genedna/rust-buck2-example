b2a7b279ac3337b127c16341595ec62da1c720f9
Quanyi Ma <eli@patch.sh>
2024-02-14 14:11:59 +0800

    添加 reindeer 生成 BUCK 的测试
    Signed-off-by: Quanyi Ma <eli@patch.sh>

---
+++ README.md
@@ -0,0 +1,2 @@
+## rust-buck2-example 是一个用于演示如何使用 buck2 构建 rust 项目的示例项目。
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use rand::{thread_rng, Rng};
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = thread_rng();
+    let x: u32 = rng.gen();
+
+    println!("A random value: {}", x);
 }
